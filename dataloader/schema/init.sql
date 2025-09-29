9ea305537f620895e991a4bca526f4a684cf311b
khangluong2004 <khangluong2004@gmail.com>
2025-09-29 20:40:35 +1000

    feat: Done db schema

--- dataloader/schema/init.sql
+++ dataloader/schema/init.sql
@@ -7,9 +7,55 @@ CREATE TABLE app_user
 (
     user_id      UUID PRIMARY KEY,
     email        TEXT      NOT NULL,
+    name         TEXT,
     saltpassword TEXT      NOT NULL,
-    name         TEXT      NOT NULL,
-    region       TEXT,
-    location     TEXT,
-    created_at   TIMESTAMP NOT NULL
+    created_at   TIMESTAMP NOT NULL,
+    access_token TEXT,
+    refresh_token TEXT
 );
+
+CREATE TABLE knowledge
+(
+    knowledge_id UUID PRIMARY KEY,
+    creator_id  UUID REFERENCES app_user,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    embedding VECTOR(1024) NOT NULL
+);
+
+CREATE TABLE email
+(
+    email_id UUID PRIMARY KEY,
+    user_id UUID REFERENCES app_user,
+    from_sender TEXT,
+    email_subject TEXT,
+    email_status TEXT,
+    email_timestamp TEXT,
+    full_content TEXT,
+    summary TEXT,
+    draft_response TEXT
+);
+
+CREATE TABLE task
+(
+    task_id UUID PRIMARY KEY,
+    user_id UUID REFERENCES app_user,
+    title TEXT,
+    task_description TEXT,
+    task_priority TEXT,
+    due_date TIMESTAMP,
+    completed TEXT,
+    email_id UUID REFERENCES email,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE invoice
+(
+    invoice_id UUID PRIMARY KEY,
+    creator_id UUID REFERENCES app_user,
+    full_name TEXT,
+    phone_number TEXT,
+    consumer_address TEXT,
+    item_name TEXT,
+    item_cost REAL
+)
---
+++ server/app/routers/email.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import Response, Depends, HTTPException, status, Request
+
+from app.config import get_settings
+from app.db.connection import SessionDep
+from app.dependencies import get_current_user
+from app.schemas.app import User, BaseEmail, Email
+
+router = APIRouter(tags=["email"])
+
+
+@router.get("/emails/add_email")
+async def add_email(
+    current_user: Annotated[User, Depends(get_current_user)],
+    session: SessionDep,
+    email: BaseEmail
+) -> Email:
+    """
+    Retrieves the currently logged in user
+    """
+
+    new_email = Email(**email.model_dump(), user_id=current_user.user_id)
+    session.add(new_email)
+    session.commit()
+    return new_email
